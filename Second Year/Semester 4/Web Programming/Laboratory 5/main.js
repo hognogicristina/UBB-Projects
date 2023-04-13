620265ec4ad89ee815575fb6177d1f728ce9c767
Hognogi Cristina <hognogicristina@gmail.com>
2023-04-13 16:49:08 +0300

    Working on assignments..

--- Second Year/Semester 4/Web Programming/Laboratory 5/main.js
+++ Second Year/Semester 4/Web Programming/Laboratory 5/main.js
@@ -1,22 +1,43 @@
 function moveRight() {
-    var left = document.getElementById("leftList");
+    var left = document.getElementById("leftList")
     var selectedItem = left.selectedIndex;
 
-    var right = document.getElementById("rightList");
-    var opt = left[selectedItem].cloneNode(true);
-    left.removeChild(left[selectedItem]);
-    right.appendChild(opt);
+    var right = document.getElementById("rightList")
+    var opt = left[selectedItem].cloneNode(true)
+    left.removeChild(left[selectedItem])
+    right.appendChild(opt)
 }
 
 function moveLeft() {
-    var right = document.getElementById("rightList");
-    var selectedItem = right.selectedIndex;
+    var right = document.getElementById("rightList")
+    var selectedItem = right.selectedIndex
 
-    var left = document.getElementById("leftList");
-    var opt = right[selectedItem].cloneNode(true);
-    right.removeChild(right[selectedItem]);
-    left.appendChild(opt);
+    var left = document.getElementById("leftList")
+    var opt = right[selectedItem].cloneNode(true)
+    right.removeChild(right[selectedItem])
+    left.appendChild(opt)
 }
+
+function moveAllLeftToRight() {
+    var leftList = document.getElementById("leftList")
+    var rightList = document.getElementById("rightList")
+
+    while (leftList.options.length > 0) {
+        rightList.add(leftList.options[0])
+    }
+}
+
+function moveAllRightToLeft() {
+    var leftList = document.getElementById("leftList")
+    var rightList = document.getElementById("rightList")
+
+    while (rightList.options.length > 0) {
+        leftList.add(rightList.options[0])
+    }
+}
+
+document.getElementById("rightButton").addEventListener("click", moveAllLeftToRight)
+document.getElementById("leftButton").addEventListener("click", moveAllRightToLeft)
 
 document.getElementById("leftList").addEventListener("dblclick", moveRight)
 document.getElementById("rightList").addEventListener("dblclick", moveLeft)
--- Second Year/Semester 4/Web Programming/Laboratory 6/jqery.js
+++ Second Year/Semester 4/Web Programming/Laboratory 6/jqery.js
@@ -1,9 +1,31 @@
 $(document).ready(function () {
+    const menuData = [
+        // this menuData is an array of objects
+        { name: 'Menu 1', submenu: ['Submenu 1', 'Submenu 2', 'Submenu 3'] },
+        { name: 'Menu 2', submenu: ['Submenu 1', 'Submenu 2'] },
+        { name: 'Menu 3', submenu: ['Submenu 1', 'Submenu 2', 'Submenu 3', 'Submenu 4'] }
+    ]
+    
+    // this function creates a link with text
+    const createLink = text => $('<a>').attr('href', '#').text(text)
+    
+    // this function creates a menu item with submenu
+    const createMenuItem = ({ name, submenu }) => {
+    const submenuItems = submenu.map(createLink)
+    const submenuList = $('<ul>').addClass('submenu').append(submenuItems)
+    return $('<li>').append(createLink(name), submenuList)
+    }
+    
+    // create menu and append it to nav
+    const menuItems = menuData.map(createMenuItem)
+    const menu = $('<ul>').addClass('menu').append(menuItems)
+    $('nav').append(menu)
+
     // set styles for nav and menu
     $('nav').css({
         'background-color': '#333',
         'padding': '10px 0'
-    });
+    })
     $('.menu').css({
         'list-style': 'none',
         'margin': '0',
@@ -12,19 +34,19 @@ $(document).ready(function () {
         'justify-content': 'space-between',
         'max-width': '900px',
         'margin': '0 auto'
-    });
+    })
 
     // set styles for menu items
     $('.menu li').css({
         'position': 'relative',
         'margin': '0 20px'
-    });
+    })
     $('.menu a').css({
         'display': 'block',
         'padding': '10px',
         'color': '#fff',
         'text-decoration': 'none'
-    });
+    })
 
     // set styles for submenus
     $('.submenu').css({
@@ -37,23 +59,23 @@ $(document).ready(function () {
         'list-style': 'none',
         'background-color': '#333',
         'display': 'none'
-    });
+    })
     $('.submenu li').css({
         'margin': '10px 0'
-    });
+    })
     $('.submenu a').css({
         'padding': '5px 0'
-    });
+    })
 
     // show/hide submenu on click
     $('.menu li').on('click', function () {
-        var submenu = $(this).find('.submenu');
-        submenu.slideToggle();
-    });
+        var submenu = $(this).find('.submenu')
+        submenu.slideToggle()
+    })
 
     // add active class on click
     $('.menu li').on('click', function () {
-        $(this).toggleClass('active');
-        $(this).siblings().removeClass('active');
-    });
-});
+        $(this).toggleClass('active')
+        $(this).siblings().removeClass('active')
+    })
+})
