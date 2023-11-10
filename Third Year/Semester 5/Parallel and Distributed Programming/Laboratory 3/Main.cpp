c91bd433ca9cb0278ec9167ab8729ee25fd781d8
Cristina Hognogi <90508610+hognogicristina@users.noreply.github.com>
2023-11-10 09:07:17 +0200

    Working on assignments..

--- Third Year/Semester 5/Parallel and Distributed Programming/Laboratory 3/Main.cpp
+++ Third Year/Semester 5/Parallel and Distributed Programming/Laboratory 3/Main.cpp
@@ -5,6 +5,7 @@
 #include <random>
 #include <stdexcept>
 
+using namespace std;
 const int THREAD_COUNT = 10;
 
 // Class for representing a matrix
@@ -12,33 +13,24 @@ class Matrix {
 private:
     int rows_;
     int columns_;
-    std::vector<std::vector<int>> matrix_;
+    vector<vector<int>> matrix_; // The matrix is represented as a vector of vectors
     const int MAX_INITIAL_VALUE = 100;
     const int MIN_INITIAL_VALUE = 10;
 
 public:
     Matrix(int rows, int columns) : rows_(rows), columns_(columns) {
-        matrix_ = std::vector<std::vector<int>>(rows, std::vector<int>(columns, 0));
+        matrix_ = vector<vector<int>>(rows, vector<int>(columns, 0));
     }
 
     // Fill the matrix with random values
     void fillMatrixRandomly() {
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<int> distribution(MIN_INITIAL_VALUE, MAX_INITIAL_VALUE);
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<int> distribution(MIN_INITIAL_VALUE, MAX_INITIAL_VALUE);
 
         for (int i = 0; i < rows_; i++) {
             for (int j = 0; j < columns_; j++) {
                 matrix_[i][j] = distribution(gen);
-            }
-        }
-    }
-
-    // Reset all elements in the matrix to 0
-    void reset() {
-        for (int i = 0; i < rows_; i++) {
-            for (int j = 0; j < columns_; j++) {
-                matrix_[i][j] = 0;
             }
         }
     }
@@ -56,7 +48,7 @@ public:
     // Get the element at a specific position in the matrix
     int getElementOnPosition(int x, int y) const {
         if (x < 0 || x >= rows_ || y < 0 || y >= columns_) {
-            throw std::out_of_range("Invalid coordinates");
+            throw out_of_range("Invalid coordinates");
         }
         return matrix_[x][y];
     }
@@ -64,13 +56,13 @@ public:
     // Set the element at a specific position in the matrix
     void setElementOnPosition(int x, int y, int newValue) {
         if (x < 0 || x >= rows_ || y < 0 || y >= columns_) {
-            throw std::out_of_range("Invalid coordinates");
+            throw out_of_range("Invalid coordinates");
         }
         matrix_[x][y] = newValue;
     }
 
     // Overload the << operator to print the matrix
-    friend std::ostream &operator<<(std::ostream &os, const Matrix &matrix) {
+    friend ostream &operator<<(ostream &os, const Matrix &matrix) {
         for (int i = 0; i < matrix.rows_; i++) {
             for (int j = 0; j < matrix.columns_; j++) {
                 os << matrix.matrix_[i][j] << ' ';
@@ -105,6 +97,7 @@ public:
             : m1_(m1), m2_(m2), result_(result), startRow_(startRow), startColumn_(startColumn), positions_(positions) {}
 
     // Overload the () operator to define the thread's execution
+    // The thread will compute the product of the matrices on the positions it is assigned
     void operator()() {
         int currentRow = startRow_;
         int currentColumn = startColumn_;
@@ -121,18 +114,21 @@ public:
 };
 
 // Generate tasks for matrix multiplication
-std::vector<MultiplierThread> generateTasks(const Matrix &m1, const Matrix &m2, Matrix &result) {
-    std::vector<MultiplierThread> threads;
-    int positions = result.getColumns() * result.getRows();
-    int basePositionsPerThread = positions / THREAD_COUNT;
-    int threadsWithAnExtraPosition = positions % THREAD_COUNT;
+vector<MultiplierThread> generateTasks(const Matrix &m1, const Matrix &m2, Matrix &result) {
+    vector<MultiplierThread> threads; // The threads will be stored in a vector
+    int positions = result.getColumns() * result.getRows(); // The number of positions in the result matrix
+    int basePositionsPerThread = positions / THREAD_COUNT; // The number of positions each thread will compute
+    int threadsWithAnExtraPosition = positions % THREAD_COUNT; // The number of threads that will compute an extra position
     int coveredPositions = 0;
 
     for (int threadIndex = 0; threadIndex < THREAD_COUNT; threadIndex++) {
+        // Compute the number of positions the current thread will compute
         int positionsForCurrentThread = basePositionsPerThread;
+
         if (threadIndex < threadsWithAnExtraPosition) {
             positionsForCurrentThread++;
         }
+
         threads.emplace_back(m1, m2, result, coveredPositions / result.getColumns(), coveredPositions % result.getColumns(),
                              positionsForCurrentThread);
         coveredPositions += positionsForCurrentThread;
@@ -142,30 +138,51 @@ std::vector<MultiplierThread> generateTasks(const Matrix &m1, const Matrix &m2, 
 }
 
 int main() {
-    // Create matrices and initialize them with random values
-    Matrix m1(4, 3);
-    Matrix m2(3, 7);
-    Matrix result(m1.getRows(), m2.getColumns());
+    // Matrices initialization
+    Matrix m1(2000, 2000), m2(2000, 2000), m3(100, 100), m4(100, 100), m5(50, 50), m6(50, 50);
+    Matrix result1(m1.getRows(), m2.getColumns()), result2(m3.getRows(), m4.getColumns()), result3(m5.getRows(), m6.getColumns());
 
     m1.fillMatrixRandomly();
     m2.fillMatrixRandomly();
+    m3.fillMatrixRandomly();
+    m4.fillMatrixRandomly();
+    m5.fillMatrixRandomly();
+    m6.fillMatrixRandomly();
 
-    // Generate tasks for matrix multiplication
-    std::vector<MultiplierThread> tasks = generateTasks(m1, m2, result);
+    // Perform matrix multiplication and measure the time taken for each operation
+    auto performMultiplication = [](const Matrix& m1, const Matrix& m2, Matrix& result, int matrixNumber) -> void {
+        auto tasks = generateTasks(m1, m2, result);
+        vector<thread> threads;
+        threads.reserve(tasks.size()); // Reserve space to prevent reallocations
 
-    // Create threads and execute the tasks
-    std::vector<std::thread> threads;
-    for (auto &task: tasks) {
-        threads.emplace_back(task);
-    }
+        chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+        for (auto& task : tasks) {
+            threads.push_back(thread(task));
+        }
+        chrono::steady_clock::time_point thread_creation_end = chrono::steady_clock::now();
 
-    for (auto &thread: threads) {
-        thread.join();
-    }
+        for (auto& thread : threads) {
+            if (thread.joinable()) {
+                thread.join();
+            }
+        }
+        chrono::steady_clock::time_point end = chrono::steady_clock::now();
 
-    // Print the result matrix
-    std::cout << "Result:" << std::endl;
-    std::cout << result << std::endl;
+        cout << "Result matrix " << matrixNumber << ":" << endl;
+        cout << result << endl;
+        cout << "Thread creation for matrix " << matrixNumber << " took "
+                  << chrono::duration_cast<chrono::microseconds>(thread_creation_end - begin).count()
+                  << " microseconds." << endl;
+        cout << "Total time for matrix multiplication " << matrixNumber << " took "
+                  << chrono::duration_cast<chrono::microseconds>(end - begin).count()
+                  << " microseconds." << endl;
+        cout << endl;
+    };
+
+    // Perform multiplications and display results
+    performMultiplication(m1, m2, result1, 1);
+    performMultiplication(m3, m4, result2, 2);
+    performMultiplication(m5, m6, result3, 3);
 
     return 0;
 }
