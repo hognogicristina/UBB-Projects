99e191b3b6d89834b4ef1b61dedb648e3332c370
Cristina Hognogi <90508610+hognogicristina@users.noreply.github.com>
2023-11-18 12:25:17 +0200

    Working on assignments..

--- Third Year/Semester 5/Public-key Cryptography/Laboratory 3/Main.cpp
+++ Third Year/Semester 5/Public-key Cryptography/Laboratory 3/Main.cpp
@@ -1,50 +1,116 @@
 #include <iostream>
-#include <cmath>
+#include <vector>
 #include <cstdlib>
-#include <algorithm>
+#include <ctime>
 
-long long gcd(long long a, long long b) {
-    while (b != 0) {
-        long long t = b;
-        b = a % b;
-        a = t;
+using namespace std;
+
+int computeGcd(int x, int y) {
+    // This function implements the Euclidean algorithm to compute GCD
+    if (x == 0) {
+        return y;
     }
-    return a;
+    return computeGcd(y % x, x);
 }
 
-long long modpow(long long base, long long exp, long long modulus) {
-    base %= modulus;
-    long long result = 1;
-    while (exp > 0) {
-        if (exp & 1) result = (result * base) % modulus;
-        base = (base * base) % modulus;
-        exp >>= 1;
+int computeLcm(int x, int y) {
+    // This function takes two integers and returns the LCM
+    int g = computeGcd(x, y);
+    return (x * y) / g;
+}
+
+int computeLcmList(vector<int>& numbers) {
+    // This function takes a vector of integers and returns the LCM of all the numbers
+    if (numbers.empty()) {
+        return -1;
+    } else if (numbers.size() == 1) {
+        return numbers[0];
+    } else {
+        int lcm = computeLcm(numbers[0], numbers[1]);
+        for (size_t i = 2; i < numbers.size(); i++) {
+            lcm = computeLcm(lcm, numbers[i]);
+        }
+        return lcm;
+    }
+}
+
+vector<int> binaryDecomposition(int k) {
+    // This function takes an integer and returns a vector of the orders of powers of 2 that compose it
+    // e.g. 13 = 1101
+    // returns {0, 2, 3}
+    vector<int> decomposition;
+    while (k > 0) {
+        if (k % 2 == 1) {
+            decomposition.push_back(1);
+        } else {
+            decomposition.push_back(0);
+        }
+        k = k / 2;
+    }
+    return decomposition;
+}
+
+int computeModularSquaring(int a, int k, int n) {
+    // This function takes an integer a, an integer k, and an integer n and returns a^k (mod n)
+    vector<int> sum = binaryDecomposition(k);
+    vector<int> rests;
+
+    int stop = sum.back();
+    int aux = a % n;
+    for (int i = 0; i <= stop; i++) {
+        if (find(sum.begin(), sum.end(), i) != sum.end()) {
+            rests.push_back(aux);
+        }
+        aux = (aux * aux) % n;
+    }
+    int result = 1;
+    for (int i : rests) {
+        result = (result * i) % n;
     }
     return result;
 }
 
-long long pollards_p_minus_1(long long N, long long B = 10) {
-    long long a = 2; // You can choose other values for a
-    for (long long j = 2; j < B; ++j) {
-        a = modpow(a, j, N);
+int pollardP1(int n, int B = 13, int a = 2) {
+    // This function takes an integer n and returns a non-trivial factor of n
+    // B is the bound
+    // a is the base
+    vector<int> k_list;
+    for (int i = 1; i <= B; i++) {
+        k_list.push_back(i);
     }
-    long long d = gcd(a - 1, N);
-    if (d > 1 && d < N) return d;
-    return 1; // returns 1 if no factor found
+    int k = computeLcmList(k_list);
+    if (a != 2) {
+        a = rand() % (n - 2) + 2;
+    }
+    a = computeModularSquaring(a, k, n);
+    int d = computeGcd(a - 1, n);
+    return d;
+}
+
+int getNontrivialFactor(int n) {
+    int B;
+    cout << "Enter the bound: ";
+    cin >> B;
+
+    if (B == 0) {
+        int f = pollardP1(n);
+        return f;
+    } else {
+        int f = pollardP1(n, B);
+        if (f == 1 || f == n) {
+            cout << "Failure" << endl;
+            return getNontrivialFactor(n);
+        } else {
+            cout << "Nontrivial factor found: " << f << " => " << n << " = " << f << " * " << (n / f) << endl;
+            return f;
+        }
+    }
 }
 
 int main() {
-    long long N;
-    long long B;
-    std::cout << "Enter the number N to factorize: ";
-    std::cin >> N;
-    std::cout << "Enter the bound B (0 for default): ";
-    std::cin >> B;
-    if (B <= 0) B = 10; // Default bound
-    long long factor = pollards_p_minus_1(N, B);
-    if (factor > 1)
-        std::cout << "A non-trivial factor of " << N << " is " << factor << std::endl;
-    else
-        std::cout << "No non-trivial factor found for " << N << " with bound " << B << std::endl;
+    int n;
+    cout << "Enter an odd composite number: ";
+    cin >> n;
+    int f = getNontrivialFactor(n);
     return 0;
 }
