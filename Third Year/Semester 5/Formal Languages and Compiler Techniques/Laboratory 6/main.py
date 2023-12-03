bc3e1a31671484a41bf789ad7bc8cb8a2671a805
Cristina Hognogi <90508610+hognogicristina@users.noreply.github.com>
2023-12-03 23:45:28 +0200

    Working on assignments..

---
+++ Third Year/Semester 5/Formal Languages and Compiler Techniques/Laboratory 6/main.py
@@ -0,0 +1,38 @@
+from Grammar import Grammar
+from Parser import Parser
+
+
+def main():
+    while True:
+        print("Choose an option:")
+        print("1. Run Parser 1")
+        print("2. Run Parser 2")
+        print("3. Run Parser 3")
+        print("0. Quit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            sequence_file = "seq1.txt"
+            grammar_file = "g1.txt"
+        elif choice == '2':
+            sequence_file = "seq2.txt"
+            grammar_file = "g2.txt"
+        elif choice == '3':
+            sequence_file = "seq3.txt"
+            grammar_file = "g3.txt"
+        elif choice == '0':
+            break
+        else:
+            print("Invalid choice. Please select a valid option.")
+            continue
+
+        grammar = Grammar()
+        grammar.read_from_file(grammar_file)
+
+        parser = Parser(grammar, sequence_file)
+        parser.run()
+
+
+if __name__ == "__main__":
+    main()
