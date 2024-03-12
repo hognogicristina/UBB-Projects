f65e32a3005498276ea98fb9f053ba8f5177ccc5
Cristina Hognogi <90508610+hognogicristina@users.noreply.github.com>
2024-03-12 13:14:22 +0200

    Working on assignments..

---
+++ Third Year/Semester 6/Numerical Calculus/Laboratory 3/divided_diff.m
@@ -0,0 +1,11 @@
+﻿% 1. divided differences table simple nodes
+% input: x=[0,1/6,1/2]; f = [0,1/2,1]; divided_diff(x, f)
+
+function t = divided_diff(x, f)
+    n = length(x);
+    t = zeros(n);
+    t(:,1) = f';
+    for k = 2:n
+        t(1:n-k+1, k) = diff(t(1:n-k+2, k-1))./(x(k:n)-x(1:n-k+1))';
+    end
+end
---
+++ Third Year/Semester 6/Numerical Calculus/Laboratory 3/divided_diff2.m
@@ -0,0 +1,16 @@
+﻿% 2. divided differences table double nodes
+% input: x=[-1,1]; f=[-3,1]; df=[10,2]; [z,t]=divided_diff2(x,f,df)
+
+function [z, t] = divided_diff2(x, f, df)
+	n = length(x);
+	z = repelem(x, 2);
+	nz = 2 * n;
+	z = repelem(x, 2);
+	t = zeros(nz);
+	t(:,1) = repelem(f, 2)';
+	t(1:2:nz-1, 2) = df';
+	t(2:2:nz-2, 2) = (diff(f)./diff(x))';
+	for k = 3:nz
+		t(1:nz-k+1, k) = diff(t(1:nz-k+2, k-1))./ (z(k:nz)-z(1:nz-k+1))';
+	end
+end
