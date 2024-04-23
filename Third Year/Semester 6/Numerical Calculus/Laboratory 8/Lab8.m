d39bfb78520de927ee2b85b333c73f5e53d92dc2
Cristina Hognogi <90508610+hognogicristina@users.noreply.github.com>
2024-04-23 12:48:14 +0300

    lab

--- Third Year/Semester 6/Numerical Calculus/Laboratory 8/Lab8.m
+++ Third Year/Semester 6/Numerical Calculus/Laboratory 8/Lab8.m
@@ -1,13 +1,13 @@
 ﻿% 1
 xi = [0, 1, 2];
 fi = 1./(1 + xi);
-d = divdiff(xi, fi);
+d = divided_diff(xi, fi);
 xx = linspace(0, 2, 100);
 Lf = newton_int(xi, d, xx);
 plot(xx, Lf)
 
 dfi = -1./(1+xi).^2;
-[zi, d2] = divdiff2(xi, fi, dfi);
+[zi, d2] = divided_diff2(xi, fi, dfi);
 Hf = newton_int(zi, d2, xx);
 
 hold on;
@@ -19,11 +19,11 @@ fi = [0, 225, 383, 623, 993]; % distance
 dfi = [0, 77, 80, 74, 72]; % velocity
 
 x = 10;
-[zi, d2] = divdiff2(xi, fi, dfi);
+[zi, d2] = divided_diff2(xi, fi, dfi);
 pos = newton_int(zi, d2, x)
 
-d = divdiff(xi, dfi);
+d = divided_diff(xi, dfi);
 newton_int(xi, d, x)
 
-d3 = divdiff(fi, dfi);
+d3 = divided_diff(fi, dfi);
 newton_int(fi, d3, pos)
---
+++ Third Year/Semester 6/Numerical Calculus/Laboratory 9/Lab9.m
@@ -0,0 +1,46 @@
+﻿% 1.
+xi = -2:4;
+x = linspace(-2, 4);
+fi = (xi + 1)./(3*xi.^2 + 2*xi + 1);
+plot(xi, fi, 'o');
+f = (x + 1)./(3*x.^2 + 2*xx + 1);
+hold on;
+plot(x, f);
+
+d = divided_diff(xi, fi);
+plot(x,newton_int(xi,d,x));
+
+dfi = -(3*xi.^2 + 6*xi + 1)./(3*xi.^2 + 2*xi + 1).^2;
+
+[zi, d2] = divided_diff2(xi, fi, dfi);
+plot(x, newton_int(zi, d2, x));
+
+S = spline(xi, fi, x);
+plot(x, S);
+
+
+% 2. 
+xi = [-1, -1/2, 0, 1/2, 1, 3/2];
+fi = xi.*sin(pi*xi);
+plot(xi, fi, 'o');
+x = linspace(-1, 3/2);
+f = x.*sin(pi*x);
+hold on
+plot(x, f);
+s = spline(xi, fi, x);
+plot(x, s);
+
+dfi = sin(pi*xi) + pi*xi.*cos(pi*xi);
+dc = spline(xi, [pi, fi, -1], x);
+pchip(xi, fi, x);
+
+% 3.
+
+xi = [0.5, 1.5, 2, 3, 3.5, 4.5, 5, 6, 7, 8];
+fi = [5, 5.8, 5.8, 6.8, 6.9, 7.6, 7.8, 8.2, 9.2, 9.9];
+hold on
+scatter(xi, fi);
+plot(xi, fi, 'o');
+p = polyfit(xi, fi, 1);
+plot([0.5, 8], polyval(p, [0.5, 8]))
+norm(fi - polyval(p, xi));
