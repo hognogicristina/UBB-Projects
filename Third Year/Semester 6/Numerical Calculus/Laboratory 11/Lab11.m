65cecb1331553a89415fb1e86b58bd01657027c0
Cristina Hognogi <90508610+hognogicristina@users.noreply.github.com>
2024-05-14 15:47:38 +0300

    nc and vss

---
+++ Third Year/Semester 6/Numerical Calculus/Laboratory 11/Lab11.m
@@ -0,0 +1,17 @@
+﻿% ex1.
+pi*sqrt(3)/9
+[I, nf] = romberg(@(x)1./(2 + sin(x)), 0, pi/2, 10^(-6), 50);
+
+% ex2.
+[I, n, c] = gauss_quad(@(x)(x.^2 + 1)./(x.^2 + 1), 2, 3);
+2*I;
+
+% ex3.
+for nodes = 1:5
+  gauss_quad(@(x) exp(cos(x)), nodes, 1);
+end
+integral(@(x) exp(cos(x)), 0, pi/4);
+
+% ex4.
+% b)
+gauss_quad(@(x)cos(x), 4, 5)
